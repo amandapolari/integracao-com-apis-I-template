174b8caa7bc330095d0da73f40bbb7dde2ec87ff
Amanda <amandaoliveirapolari@gmail.com>
2023-07-05 20:57:16 -0300

    Mais uma prática feita

--- src/Componentes/Usuario/Usuario.js
+++ src/Componentes/Usuario/Usuario.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const User = styled.div`
     border: black 1px solid;
@@ -8,10 +9,47 @@ const User = styled.div`
     padding: 8px;
 `;
 function Usuario(props) {
+    // verificando o que vem em usuaio
+    // console.log(props.usuario);
+
     const [usuario, setUsuario] = useState(props.usuario);
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
     const [editar, setEditar] = useState(false);
+
+    useEffect(() => {
+        getUserById(props.usuario.id);
+    }, [props.usuario]);
+
+    const headers = {
+        headers: {
+            Authorization: 'amanda-polari-easley',
+        },
+    };
+
+    const getUserById = (id) => {
+        axios
+            .get(
+                `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
+                headers
+            )
+            .then((response) => {
+                // console.log('Caso de sucesso: ', response);
+                console.log('caso de sucesso', response.data);
+                setUsuario(response.data);
+                // console.log('Caso de sucesso: ', response.data);
+                // passando as infos recebidas pela api para o estado 'usuarios'
+                // setUsuarios(response.data);
+            })
+            .catch((err) => {
+                // todo erro:
+                console.log('Caso de erro: ', err);
+                // mensagem de erro para o usuário:
+                // console.log('Caso de erro: ', err.message);
+                // mensagem de erro que o back escreveu para o front:
+                // console.log('Caso de erro: ', err.response.data);
+            });
+    };
 
     return (
         <User>
